77a642d541fcf648929c87bf6c9e25d99cde5bfa
Praveen Sharma <piyush20112@gmail.com>
2025-03-04 14:13:53 +0530

    [Praveen] ADD: Odd Even Array

--- OddEven.java
+++ OddEven.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class OddEvenArrays {
+public class OddEven {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
