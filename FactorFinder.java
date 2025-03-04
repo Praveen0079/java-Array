55f08e0ebf6d9ae1b027fa62bfa9f3f626fc98b2
Praveen Sharma <piyush20112@gmail.com>
2025-03-04 14:15:08 +0530

    [Praveen] ADD: Find factors of a number

---
+++ FactorFinder.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class FactorFinder {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        scanner.close();
+
+        int[] factors = new int[number]; // Allocate array with max possible size
+        int index = 0;
+
+        for (int i = 1; i <= number; i++) {
+            if (number % i == 0) {
+                factors[index++] = i;
+            }
+        }
+
+        System.out.print("Factors of " + number + " are: ");
+        for (int i = 0; i < index; i++) {
+            System.out.print(factors[i] + " ");
+        }
+        System.out.println();
+    }
+}
