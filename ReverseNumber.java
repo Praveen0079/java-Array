1f7fa2d7ef6ec5ae593343c70fd9cbd3ab3dc8b5
Praveen Sharma <96952903+Praveen0079@users.noreply.github.com>
2025-03-04 22:22:25 +0530

    [Praveen] ADD:  Reverse Number

--- ReverseNumber.java
+++ ReverseNumber.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class ReverseNumber {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in); 
 
         System.out.print("Enter a number: ");
         int number = scanner.nextInt();
